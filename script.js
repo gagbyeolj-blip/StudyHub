6fcbf03b1bea4cfd798a97554685fc9d1c95d0f4
ttubyeol <gagbyeolj@gmail.com>
2026-07-25 19:27:06 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-const button = document.getElementById("theme-btn");
-
 // 기본적으로 다크 모드를 위한 함수
+const button = document.getElementById("theme-btn");
+/* const (상수 변수 선언) button(id 이름) -> button이라는 id 변수 선언
+= (대입) document (현재 웹페이지)  .(접근) "get(가져오다) Element(요소) By(~로) Id(id)" */
 function updateTheme(){
     if(document.body.classList.contains("dark")){
         button.textContent="☀️ 라이트모드";
@@ -24,6 +25,7 @@ button.addEventListener("click",function () {
     updateTheme();
 });
 
+//D-DAY를 위한 함수
 function getDday(targetDate) {
     const today = new Date();
     today.setHours(0, 0, 0, 0);
@@ -36,8 +38,12 @@ function getDday(targetDate) {
 
     if(diffDays === 0) return 'D-Day';
     if(diffDays > 0) return `D-${diffDays}`;
-    return `d+${Math.ads(diffDays)}`;
+    return `d+${Math.asd(diffDays)}`;
 }
 
 //
-document.getElementById('result').textContent = getDday('2026-8-14');
+document.getElementById('results').textContent = getDday('2026-08-14'); 
+
+function alertmenu(menu){
+    alert(`오늘의 급식은 ${menu}`)
+}
